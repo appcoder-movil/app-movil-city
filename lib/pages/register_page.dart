13e96a54f9ad840d98781dd0f3d4c856690d7df9
Fernando A. Rodriguez R <softfar@gmail.com>
2022-10-25 19:34:03 -0500

    Página de registro con los campos de nombre, correo, contraseña, confirmar contraseña y botón registrar. Widget SingleChildScrollView y campo de texto para mostrar la información capturada.

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -12,6 +12,13 @@ class _RegisterPageState extends State<RegisterPage> {
   final _email=TextEditingController();
   final _password=TextEditingController();
   final _confirm_password=TextEditingController();
+  String _data = "Información del usuario";
+
+  void _onRegisterPageButtonClicked(){
+    setState(() {
+      _data="Información del usuario\nNombre: ${_name.text}\nCorreo Elactŕonico: ${_email.text}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,47 +26,70 @@ class _RegisterPageState extends State<RegisterPage> {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Image(image: AssetImage('assets/images/paisaje-urbano-logo.png')),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: _name,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Usuario'),
-                  keyboardType: TextInputType.text,
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: _email,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Correo Electrónico'),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: _password,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Contraseña'),
-                  keyboardType: TextInputType.text,
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: _confirm_password,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Confirmar Contraseña'),
-                  keyboardType: TextInputType.text,
-                ),
-              ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(image: AssetImage('assets/images/paisaje-urbano-logo.png')),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _name,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Usuario'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _email,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Correo Electrónico'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _password,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Contraseña'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    controller: _confirm_password,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Confirmar Contraseña'),
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      _onRegisterPageButtonClicked();
+                    },
+                    child: const Text('Registrar'),
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Text(
+                    _data,
+                    style: const TextStyle(
+                      fontSize: 12, fontStyle: FontStyle.italic,
+                    ),
+                  )
+                ],
+            ),
           ),
         ),
       ),
